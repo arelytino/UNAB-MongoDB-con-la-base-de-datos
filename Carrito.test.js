8eb5574c06ea78760fbbbea41424fdbfb344476e
arelytino <tl0479022012@unab.edu.sv>
2026-07-17 22:50:03 -0600

    Create Carrito.test.js
    PRIMERA ENTREGA DE AVANCE - M5

---
+++ Carrito.test.js
@@ -0,0 +1,10 @@
+CÓDIGO PARA carrito.test.js:
+const { calcularTotal } = require('./carrito');
+
+test('calcula el total de múltiples productos', () => {
+    const carrito = [
+        { precio: 800, cantidad: 1 },
+        { precio: 20, cantidad: 2 }
+    ];
+    expect(calcularTotal(carrito)).toBe(840);
+});
