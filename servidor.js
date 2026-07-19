d26f79c9a581de8f7f5dfa1ac892a6d66f4d85bc
arelytino <tl0479022012@unab.edu.sv>
2026-07-18 20:06:19 -0600

    Create servidor.js
    primera entrega de abance -M5

---
+++ servidor.js
@@ -0,0 +1,29 @@
+Borra todo lo que tienes y pega esto:
+require('dotenv').config();
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express(); // <-- ESTO FALTABA
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+const client = new MongoClient(MONGO_URI);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("✅ Conectado a MongoDB Atlas");
+    
+    app.listen(PORT, () => {
+      console.log(Servidor corriendo en http://localhost:${PORT}); // <-- comillas invertidas
+    });
+
+  } catch (error) {
+    console.error("❌ Error:", error);
+  }
+}
+
+run();
+---
+
+
